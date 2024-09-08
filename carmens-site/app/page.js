695c9c6d871024aa35e5e35c6e492a7d3033b452
McCray Austin <47298611+mmaustin@users.noreply.github.com>
2024-09-08 10:44:48 -0400

    Installed shadcn/ui and components folder

--- carmens-site/app/page.js
+++ carmens-site/app/page.js
@@ -1,8 +1,10 @@
-import Image from "next/image";
+import Header from "@/components/Header";
+
 
 export default function Home() {
   return (
-    <div className=" ">
+    <div className="">
+      <Header />
       Carmen's Fabulous Website!
     </div>
   );
---
+++ carmens-site/components/Header.jsx
@@ -0,0 +1,13 @@
+import { Button } from "./ui/button";
+
+
+const Header = () => {
+  return (
+    <div>
+      <Button>
+        press play
+      </Button>
+    </div>
+  )
+}
+export default Header;
