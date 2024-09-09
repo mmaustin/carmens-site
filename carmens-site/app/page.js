7a07582bba96d3194fb5c41852ca2e39bde902f2
McCray Austin <47298611+mmaustin@users.noreply.github.com>
2024-09-09 14:54:30 -0400

    Test code for various tailwind effects

--- carmens-site/app/page.js
+++ carmens-site/app/page.js
@@ -1,11 +1,14 @@
 import Header from "@/components/Header";
 
 
+
+
 export default function Home() {
   return (
-    <div className="">
+    <div className=" text-secondary-foreground container flex flex-col">
       <Header />
       Carmen's Fabulous Website!
+      <h1 className="border border-orange-900">font weight</h1>
     </div>
   );
 }
--- carmens-site/components/Header.jsx
+++ carmens-site/components/Header.jsx
@@ -1,10 +1,12 @@
 import { Button } from "./ui/button";
 
+Button
+
 
 const Header = () => {
   return (
-    <div>
-      <Button>
+    <div className="border border-blue-600 bg-chart-5">
+      <Button variant="destructive" size="icon" className="border border-blue-700 text-red-700" >
         press play
       </Button>
     </div>
