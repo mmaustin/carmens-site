0ee8d280349a44e6361a746879b19f4ebdf701ae
McCray Austin <47298611+mmaustin@users.noreply.github.com>
2024-09-10 14:37:39 -0400

    Mapped over links array in navbar component

--- carmens-site/components/Header.jsx
+++ carmens-site/components/Header.jsx
@@ -1,11 +1,9 @@
-import { Button } from "./ui/button";
 
-Button
 
 
 const Header = () => {
   return (
-    <div className="border border-blue-600 bg-chart-5 sticky top-0 opacity-70">
+    <div className="border border-blue-600 md:border-red-700 lg:border-blue-700 bg-chart-5 sticky top-0 opacity-70">
       <p className="px-8">Some Stuff</p>
       <p className="px-8">Some Stuff</p>
       <p className="px-8">Some Stuff</p>
--- carmens-site/components/Navbar.jsx
+++ carmens-site/components/Navbar.jsx
@@ -1,9 +1,34 @@
+'use client';
+import Link from "next/link";
+import { usePathname } from "next/navigation";
 
-
+const links = [
+  {
+    name: "home",
+    path: '/'
+  },
+  {
+    name: "books",
+    path: '/books'
+  },
+  {
+    name: "about",
+    path: '/about'
+  }
+];
 
 const Navbar = () => {
+
+  const pathName = usePathname();
+
   return (
-    <div>Navbar</div>
+    <nav className="flex gap-8">
+      {links.map((link, i) => {
+        return (
+          <Link key={i} href={link.path} className={`${link.path === pathName && "text-accent border-b-2 border-accent"} capitalize font-medium hoover:text-accent`}>{link.name}</Link>
+        )
+      })}
+    </nav>
   )
 }
 export default Navbar;
