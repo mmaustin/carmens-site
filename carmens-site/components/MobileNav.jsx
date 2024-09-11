b6d0703312e62a02ff691a32da9d7c884436c542
McCray Austin <47298611+mmaustin@users.noreply.github.com>
2024-09-11 16:01:53 -0400

    Imported shadci, react-icons, and radix-ui components into mobilenave component

---
+++ carmens-site/components/MobileNav.jsx
@@ -0,0 +1,11 @@
+import { Sheet, SheetContent, SheetTrigger, SheetTitle, SheetDescription, SheetClose } from "./ui/sheet";
+import { CiMenuFries } from "react-icons/ci";
+import * as VisuallyHidden from "@radix-ui/react-visually-hidden";
+
+
+const MobileNav = () => {
+  return (
+    <div>MobileNav</div>
+  )
+}
+export default MobileNav;
