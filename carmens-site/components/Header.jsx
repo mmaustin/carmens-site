09887f75dffc110f8ad3c2119a139da69778a405
McCray Austin <47298611+mmaustin@users.noreply.github.com>
2024-09-15 18:34:58 -0400

    Added flex properties to navbar component elements

--- carmens-site/components/Header.jsx
+++ carmens-site/components/Header.jsx
@@ -4,8 +4,8 @@ import Navbar from "./Navbar";
 
 const Header = () => {
   return (
-    <header className="flex justify-center items-center sticky top-0 bg-chart-5/20 opacity-80 h-28">
-      <div className="container border border-red-700  hidden lg:flex items-center gap-8">
+    <header className="flex justify-center items-center sticky top-0 opacity-80 h-28">
+      <div className="container hidden lg:flex items-center gap-8 rounded-full">
         <Navbar />
       </div>
       <div className="lg:hidden mt-6 md:mt-0">
--- carmens-site/components/Navbar.jsx
+++ carmens-site/components/Navbar.jsx
@@ -22,13 +22,18 @@ const Navbar = () => {
   const pathName = usePathname();
 
   return (
-    <nav className="flex gap-8">
-      {links.map((link, i) => {
-        return (
-          <Link key={i} href={link.path} className={`${link.path === pathName && " text-green-500 border-b-2 border-accent"} capitalize font-medium hoover:text-accent`}>{link.name}</Link>
-        )
-      })}
-    </nav>
+    <div className="flex justify-around w-full border">
+      <h3>
+        <Link href={'/'} className="text-3xl text-green-500 border capitalize" >CARMEN e</Link>
+      </h3>
+      <nav className="flex gap-8 border">
+        {links.map((link, i) => {
+          return (
+            <Link key={i} href={link.path} className={`${link.path === pathName && " text-green-500 border-b-2 border-accent"} capitalize font-medium hoover:text-accent`}>{link.name}</Link>
+          )
+        })}
+      </nav>
+    </div>
   )
 }
 export default Navbar;
