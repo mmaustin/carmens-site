a526cafe5fab899250354f00a4f21e0dfce6c719
McCray Austin <47298611+mmaustin@users.noreply.github.com>
2024-09-15 18:16:39 -0400

    Added flex properties to the header element in the header component

--- carmens-site/components/Header.jsx
+++ carmens-site/components/Header.jsx
@@ -4,11 +4,8 @@ import Navbar from "./Navbar";
 
 const Header = () => {
   return (
-    <header className="border sticky top-0 bg-chart-5/20 opacity-80 h-28">
-      {/* <p className="px-8">Some Stuff</p>
-      <p className="px-8">Some Stuff</p>
-      <p className="px-8">Some Stuff</p> */}
-      <div className="hidden lg:flex items-center gap-8">
+    <header className="flex justify-center items-center sticky top-0 bg-chart-5/20 opacity-80 h-28">
+      <div className="container border border-red-700  hidden lg:flex items-center gap-8">
         <Navbar />
       </div>
       <div className="lg:hidden mt-6 md:mt-0">
