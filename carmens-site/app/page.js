315771ac10f945040de29347e93c97bc8f5c5ff0
McCray Austin <47298611+mmaustin@users.noreply.github.com>
2024-09-20 13:24:54 -0400

    Created temporary iterable books array to be used in swiper

--- carmens-site/app/page.js
+++ carmens-site/app/page.js
@@ -7,14 +7,37 @@ import { Tooltip, TooltipContent, TooltipProvider, TooltipTrigger } from "@/comp
 import Link from "next/link";
 import Image from "next/image";
 import img from '@/public/gone-girl.jpg';
+import BookSliderButtons from "@/components/BookSliderButtons";
+import { isPagesAPIRouteMatch } from "next/dist/server/future/route-matches/pages-api-route-match";
+import { LocaleRouteNormalizer } from "next/dist/server/future/normalizers/locale-route-normalizer";
 
+const projects = [
+  {
+    title: "uptown menace",
+    description: "Lorem ipsum dolor sit amet consectetur adipisicing elit.Asperiores sed vel quis at ab.Vero incidunt dolore, repellendus aperiam officiis vitae doloremque magni deserunt laborum, modi recusandae labore unde praesentium!",
+    image: img,
+    live: "https://github.com/mmaustin",
+  },
+  {
+    title: "turning back time",
+    description: "Lorem ipsum dolor sit amet consectetur adipisicing elit.Asperiores sed vel quis at ab.Vero incidunt dolore, repellendus aperiam officiis vitae doloremque magni deserunt laborum, modi recusandae labore unde praesentium!",
+    image: img,
+    live: "https://github.com/mmaustin",
+  },
+  {
+    title: "abuelita . . . ",
+    description: "Lorem ipsum dolor sit amet consectetur adipisicing elit.Asperiores sed vel quis at ab.Vero incidunt dolore, repellendus aperiam officiis vitae doloremque magni deserunt laborum, modi recusandae labore unde praesentium!",
+    image: img,
+    live: "https://github.com/mmaustin",
+  },
+];
 
 export default function Home() {
   return (
     <div className="w-full flex flex-col">
       <div className="flex flex-col justify-center items-center ">
         <h1 className="text-3xl my-10 font-serif">
-          Uptown Menance
+
         </h1>
         <div className="flex flex-row justify-around items-center columns-2 mb-8 lg:mb-0 rounded-xl w-full">
           <div>
