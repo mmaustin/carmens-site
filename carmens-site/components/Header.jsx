0a8bb2f56bd60a22265b22b440bd01b44348e1cf
McCray Austin <47298611+mmaustin@users.noreply.github.com>
2024-09-23 11:07:21 -0400

    Added a div to house multiple elements in moblenav component

--- carmens-site/components/Header.jsx
+++ carmens-site/components/Header.jsx
@@ -7,7 +7,7 @@ const Header = () => {
       <div className="container hidden md:flex items-center gap-8 rounded-full">
         <Navbar />
       </div>
-      <div className="md:hidden mt-6 md:mt-0">
+      <div className="md:hidden mt-6 md:mt-0 w-full">
         <MobileNav />
       </div>
     </header>
--- carmens-site/components/MobileNav.jsx
+++ carmens-site/components/MobileNav.jsx
@@ -27,33 +27,35 @@ const MobileNav = () => {
 
   return (
     <Sheet>
-      <SheetTrigger className="flex justify-center items-center">
-        <CiMenuFries className="text-[32px] text-sky-700" />
-      </SheetTrigger>
-      <SheetContent className="flex items-center flex-col mt-20">
-        <SheetTitle >
-          <VisuallyHidden.Root>
-            Mobile Menu Title
-          </VisuallyHidden.Root>
-        </SheetTitle>
-        <SheetDescription >
-          <VisuallyHidden.Root>
-            Mobile Menu Description
-          </VisuallyHidden.Root>
-        </SheetDescription>
-        {/* <ScrollArea className="h-32 w-48 rounded-md border border-sky-700 mt-10"> */}
-        <nav className="mt-2 flex flex-col justify-center items-center gap-8">
-          {links.map((link, i) => {
-            return (
-              <SheetClose key={i} asChild>
-                <Link key={i} href={link.path} className={`${link.path === pathName && "text-sky-700 border-b-2 border-sky-700"} text-xl capitalize hover:text-sky-700 transition-all`}>{link.name}</Link>
-              </SheetClose>
-            )
-          })}
-        </nav>
-
-        {/* </ScrollArea> */}
-      </SheetContent>
+      <div className="w-full flex flex-row justify-around items-center">
+        <SheetTrigger className="flex justify-center items-center">
+          <CiMenuFries className="text-[32px] text-sky-700" />
+        </SheetTrigger>
+        <SheetContent className="flex items-center flex-col mt-20">
+          <SheetTitle >
+            <VisuallyHidden.Root>
+              Mobile Menu Title
+            </VisuallyHidden.Root>
+          </SheetTitle>
+          <SheetDescription >
+            <VisuallyHidden.Root>
+              Mobile Menu Description
+            </VisuallyHidden.Root>
+          </SheetDescription>
+          {/* <ScrollArea className="h-32 w-48 rounded-md border border-sky-700 mt-10"> */}
+          <nav className="mt-2 flex flex-col justify-center items-center gap-8">
+            {links.map((link, i) => {
+              return (
+                <SheetClose key={i} asChild>
+                  <Link key={i} href={link.path} className={`${link.path === pathName && "text-sky-700 border-b-2 border-sky-700"} text-xl capitalize hover:text-sky-700 transition-all`}>{link.name}</Link>
+                </SheetClose>
+              )
+            })}
+          </nav>
+          {/* </ScrollArea> */}
+        </SheetContent>
+        <h1 className="text-2xl font-serif uppercase">Carmen N.E.</h1>
+      </div>
     </Sheet>
   )
 }
