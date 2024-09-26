081b0b9f850336f71ea7b608e1be2b4e07d912fa
McCray Austin <47298611+mmaustin@users.noreply.github.com>
2024-09-26 10:35:37 -0400

    Adjusted spacing of elements in the AboutCarmen component

--- carmens-site/components/AboutCarmen.jsx
+++ carmens-site/components/AboutCarmen.jsx
@@ -9,9 +9,15 @@ const AboutCarmen = () => {
 
   if (pathName === '/') {
     return (
-      <div className="h-auto bg-red-400/80 flex flex-col md:flex-row justify-around items-center">
-        <CarmenPhoto />
-        <p className="w-1/3">Lorem ipsum dolor sit amet consectetur adipisicing elit. Vero ipsa aliquid debitis, sequi aperiam quae iusto temporibus labore accusantium praesentium fuga doloremque. Optio praesentium ab quis suscipit inventore? Dolorem, commodi.</p>
+      <div className="h-auto bg-red-400/80 flex flex-row justify-around items-center">
+        {/* <div className="border">
+        </div>
+        <div className="border">
+        </div> */}
+        <div className="">
+          <CarmenPhoto />
+        </div>
+        <p className="w-[250px] h-[250px]">Lorem ipsum dolor sit amet consectetur adipisicing elit. Vero ipsa aliquid debitis, sequi aperiam quae iusto temporibus labore accusantium praesentium fuga doloremque. Optio praesentium ab quis suscipit inventore? Dolorem, commodi.</p>
       </div>
     )
   };
--- carmens-site/components/CarmenPhoto.jsx
+++ carmens-site/components/CarmenPhoto.jsx
@@ -5,7 +5,7 @@ import img from "@/public/gone-girl.jpg"
 
 const CarmenPhoto = () => {
   return (
-    <div className="w-1/3 h-full relative">
+    <div className="">
       <div
         className="w-[250px] h-[250px] mix-blend relative backdrop-blur-md z-0">
         <Image
