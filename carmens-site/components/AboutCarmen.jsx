294a9ab3efb86f188af70d81b24c0d1d9566da33
McCray Austin <47298611+mmaustin@users.noreply.github.com>
2024-09-29 11:07:17 -0400

    Removed commented code from photo container components

--- carmens-site/components/AboutCarmen.jsx
+++ carmens-site/components/AboutCarmen.jsx
@@ -2,9 +2,6 @@
 
 import { usePathname } from "next/navigation";
 import CarmenPhoto from "./CarmenPhoto";
-import Photo from "./Photo";
-import Image from "next/image";
-import img from "@/public/gone-girl.jpg";
 
 const AboutCarmen = () => {
 
@@ -13,24 +10,10 @@ const AboutCarmen = () => {
   if (pathName === '/') {
     return (
       <div className="h-auto w-full bg-red-500/60 flex">
-        {/* <div className="border-2 flex justify-center items-center relative"> */}
-        {/* <div
-          className="flex flex-col justify-around items-center mt-10 md:mt-0 w-[75%] md:w-[250px] h-[250px] mix-blend backdrop-blur-md z-0"> */}
         <div className="h-auto w-full my-10 mx-6 gap-8 flex flex-wrap justify-around items-center">
           <div className="">
             <CarmenPhoto />
           </div>
-          {/* <Image
-            src={img}
-            fill
-            priority
-            sizes="100vw"
-            alt="carmen"
-            className="object-cover rounded-xl"
-          // height='auto'
-          // width='100%'
-          /> */}
-          {/* </div> */}
           <p className=" w-[200px] md:w-[250px] text-white flex justify-center items-center">Carmen is the bestest person in the whole wide world.  She's the bestest mother, wife, author, exercise instructor, friend, Knicks fan--sadly--and the bestest everything else too!  I suggest you buy all of her books, cause you don't want her folks from the DR to come knocking on your door!.</p>
         </div>
       </div>
