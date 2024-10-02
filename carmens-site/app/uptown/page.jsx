4169bbad52c8446f1319ab052f91ca6157183d15
McCray Austin <47298611+mmaustin@users.noreply.github.com>
2024-10-02 17:27:59 -0400

    Starting layout for the uptown page

--- carmens-site/app/uptown/page.jsx
+++ carmens-site/app/uptown/page.jsx
@@ -1,9 +1,17 @@
+import Photo from "@/components/Photo";
 
 
 
 const UptownMenace = () => {
   return (
-    <div>Uptown Menace</div>
+    <div className="container">
+      <div className="">
+        <Photo />
+      </div>
+      <div className="">
+        <p className="flex border">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Est nostrum accusamus amet ut eaque minima perferendis quae ipsa labore, rem mollitia magnam omnis veniam magni temporibus facilis dolor officia. Eaque.</p>
+      </div>
+    </div>
   )
 }
 export default UptownMenace;
