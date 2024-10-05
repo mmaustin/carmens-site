5189b8598dc0835db3d92ec014990e343c3c40df
McCray Austin <47298611+mmaustin@users.noreply.github.com>
2024-10-05 11:05:00 -0400

    Installed shadcn card component

--- carmens-site/app/uptown/page.jsx
+++ carmens-site/app/uptown/page.jsx
@@ -1,4 +1,5 @@
 import Photo from "@/components/Photo";
+import { Card, CardContent } from "@/components/ui/card";
 import { Carousel, CarouselContent, CarouselItem, CarouselNext, CarouselPrevious } from "@/components/ui/carousel";
 
 const uptownMenace = [
