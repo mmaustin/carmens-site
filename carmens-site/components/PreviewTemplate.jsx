d866846bc056fa2cefbac9448d7eda97ac796de6
McCray Austin <47298611+mmaustin@users.noreply.github.com>
2024-10-08 18:30:53 -0400

    Created a preview template component to handle all of the book preview pages

---
+++ carmens-site/components/PreviewTemplate.jsx
@@ -0,0 +1,11 @@
+import Photo from "./Photo"
+import { Card, CardContent } from "./ui/card"
+import { Carousel, CarouselContent, CarouselItem, CarouselNext, CarouselPrevious } from "./ui/carousel"
+
+
+const PreviewTemplate = (previewArray) => {
+  return (
+    <div>PreviewTemplate</div>
+  )
+}
+export default PreviewTemplate
