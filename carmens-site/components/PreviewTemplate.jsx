53cffae9bcd29b5e1c532f82100416528d017f9b
McCray Austin <47298611+mmaustin@users.noreply.github.com>
2024-10-08 18:36:12 -0400

    Added carousel and card shadcn components to preview template

--- carmens-site/components/PreviewTemplate.jsx
+++ carmens-site/components/PreviewTemplate.jsx
@@ -1,11 +1,32 @@
-import Photo from "./Photo"
-import { Card, CardContent } from "./ui/card"
-import { Carousel, CarouselContent, CarouselItem, CarouselNext, CarouselPrevious } from "./ui/carousel"
+import Photo from "./Photo";
+import { Card, CardContent } from "./ui/card";
+import { Carousel, CarouselContent, CarouselItem, CarouselNext, CarouselPrevious } from "./ui/carousel";
 
 
 const PreviewTemplate = (previewArray) => {
   return (
-    <div>PreviewTemplate</div>
+    <div className="h-auto w-full my-10 gap-8 flex flex-wrap justify-around items-center">
+      <div className="">
+        <Photo />
+      </div>
+      <Carousel className="w-1/2 max-w-sm">
+        <CarouselContent>
+          {previewArray.map((page, i) => (
+            <CarouselItem key={i}>
+              <div className="p-1">
+                <Card className="">
+                  <CardContent className="flex aspect-square items-center justify-center p-6">
+                    <span className="text-base font-serif">{page}</span>
+                  </CardContent>
+                </Card>
+              </div>
+            </CarouselItem>
+          ))}
+        </CarouselContent>
+        <CarouselPrevious />
+        <CarouselNext />
+      </Carousel>
+    </div>
   )
 }
 export default PreviewTemplate
