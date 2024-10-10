faac195f260e1b001ccbdfc08c465cd0b2832a63
McCray Austin <47298611+mmaustin@users.noreply.github.com>
2024-10-10 17:25:18 -0400

    Refactored TBT page with preview template component

--- carmens-site/app/turning/page.jsx
+++ carmens-site/app/turning/page.jsx
@@ -1,6 +1,5 @@
-import Photo from "@/components/Photo";
-import { Card, CardContent } from "@/components/ui/card";
-import { Carousel, CarouselContent, CarouselItem, CarouselNext, CarouselPrevious } from "@/components/ui/carousel";
+import PreviewTemplate from "@/components/PreviewTemplate";
+
 
 const turningBackTime = [
   "Turning Back Time dolor sit amet consectetur adipisicing elit. Officia cum minima modi eaque vitae. Quidem, autem maxime? Ratione assumenda provident, exercitationem eligendi quas, deleniti officia praesentium suscipit reprehenderit, neque vel!Lorem ipsum dolor sit amet consectetur adipisicing elit. Officia cum minima modi eaque vitae. Quidem, autem maxime? Ratione assumenda provident, exercitationem eligendi quas, deleniti officia praesentium suscipit reprehenderit, neque vel!",
@@ -18,28 +17,9 @@ const turningBackTime = [
 const TurningBackTime = () => {
 
   return (
-    <div className="h-auto w-full my-10 gap-8 flex flex-wrap justify-around items-center">
-      <div className="">
-        <Photo />
-      </div>
-      <Carousel className="w-1/2 max-w-sm">
-        <CarouselContent>
-          {turningBackTime.map((page, i) => (
-            <CarouselItem key={i}>
-              <div className="p-1">
-                <Card className="">
-                  <CardContent className="flex aspect-square items-center justify-center p-6">
-                    <span className="text-base font-serif">{page}</span>
-                  </CardContent>
-                </Card>
-              </div>
-            </CarouselItem>
-          ))}
-        </CarouselContent>
-        <CarouselPrevious />
-        <CarouselNext />
-      </Carousel>
-    </div>
+    <>
+      <PreviewTemplate previewArray={turningBackTime} />
+    </>
   );
 }
 export default TurningBackTime;
