603e82d8c8f2802becd019e9cabb6a703e8dbfe4
McCray Austin <47298611+mmaustin@users.noreply.github.com>
2024-10-10 18:03:50 -0400

    Reworked margins in previewtemplate component

--- carmens-site/components/PreviewTemplate.jsx
+++ carmens-site/components/PreviewTemplate.jsx
@@ -6,11 +6,11 @@ import { Carousel, CarouselContent, CarouselItem, CarouselNext, CarouselPrevious
 const PreviewTemplate = ({ previewArray }) => {
 
   return (
-    <div className="h-auto w-full my-10 gap-8 flex flex-wrap justify-around items-center">
-      <div className="">
+    <div className="h-auto w-full my-10 gap-14 flex flex-wrap justify-around items-center">
+      <div className="ml-6 mr-6 lg:ml-0 lg:mr-0">
         <Photo />
       </div>
-      <Carousel className="w-1/2 max-w-sm">
+      <Carousel className="w-1/2 max-w-sm ml-6 mr-6 lg:ml-0 lg:mr-0">
         <CarouselContent>
           {previewArray.map((page, i) => (
             <CarouselItem key={i}>
